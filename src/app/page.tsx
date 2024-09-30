4f2791f6c95f6b3b5ccd466397ca2d469c48893c
shirin faile <shirin.faile@hotmail.com>
2024-09-30 11:39:07 +0200

    deleted everything and gonna start over

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-import { useUserContext } from "@/context/UserContext";
-import LoginForm from "@/components/LoginForm";
-import ProfileSummary from "@/components/ProfileSummary";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 export default function Home() {
-    const { user } = useUserContext();
-  
-    return (
-      <div className="flex flex-col min-h-screen bg-gray-100">
-        <Header />
-        <main className="flex-grow flex items-center justify-center p-4">
-          {user ? <ProfileSummary /> : <LoginForm />}
-        </main>
-        <Footer />
-      </div>
-    );
-  }
+
+}
+
--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="p-4 bg-gray-800 text-white text-center">
-            <p>MealDB © 2024. All rights reserved.</p>
-        </footer>
-    );
-};
-
-export default Footer;
